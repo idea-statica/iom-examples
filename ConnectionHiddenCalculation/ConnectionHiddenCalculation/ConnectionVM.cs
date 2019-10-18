ae53fc66d085b7766bb84758af8274caead0403c
mpospisil76 <m.pospisil76@gmail.com>
2019-10-18 08:54:12 +0200

    adding the example of the hidden calculation

---
+++ ConnectionHiddenCalculation/ConnectionHiddenCalculation/ConnectionVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ConnectionHiddenCalculation
+{
+	public class ConnectionVM : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		dynamic dynamicItem;
+
+		public ConnectionVM(object item)
+		{
+			this.dynamicItem = item;
+		}
+
+		public string Name
+		{
+			get => (string)(dynamicItem.Header.Name);
+		}
+
+		public Guid ConnectionId
+		{
+			get => (Guid)(dynamicItem.Header.ConnectionID);
+		}
+
+		private void NotifyPropertyChanged(string propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
