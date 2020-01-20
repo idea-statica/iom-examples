7335e9d09e4002d07f810e4226f6a1b607fadc65
mpospisil76 <m.pospisil76@gmail.com>
2020-01-21 00:41:16 +0100

    connection hidden calculation

---
+++ ConnCalcExamples/ConHiddenCheckConsole/Program.cs
@@ -0,0 +1,59 @@
+﻿using IdeaStatiCa.Plugin;
+using System;
+using System.IO;
+
+namespace ConnCalculatorConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Start");
+
+			if(args.Length < 1)
+			{
+				Console.WriteLine("The path to Idea StatiCa installation dir is required as the argument");
+				return;
+			}
+
+			if (!Directory.Exists(args[0]))
+			{
+				Console.WriteLine(string.Format("Missing Idea StatiCa installation in '{0}'", args[0]));
+				return;
+			}
+
+			using (var calcFactory = new ConnHiddenClientFactory(args[0]))
+			{
+				var client = calcFactory.Create();
+
+				client.OpenProject("testProj.ideaCon");
+
+				// get detail about idea connection project
+				var projInfo = client.GetProjectInfo();
+
+				if (projInfo != null && projInfo.Connections != null)
+				{
+					// iterate all connections in the project
+					foreach (var con in projInfo.Connections)
+					{
+						Console.WriteLine(string.Format("Starting calculation of connection {0}", con.Identifier));
+
+						// calculate a get results for each connection in the project
+						var conRes = client.Calculate(con.Identifier);
+						Console.WriteLine("Calculation is done");
+
+						// get the geometry of the connection
+						var connectionModel = client.GetConnectionModel(con.Identifier);
+					}
+				}
+
+				client.CloseProject();
+
+				client.Close();
+			}
+
+			Console.WriteLine("End");
+			var x = Console.ReadLine();
+		}
+	}
+}
