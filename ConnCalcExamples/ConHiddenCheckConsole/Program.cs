6a617cd541dd17ddbf2ddbf8cc33cc703401bf0f
mpospisil76 <m.pospisil76@gmail.com>
2020-01-23 16:31:14 +0100

    ConnHiddenClientFactory is not disposable

--- ConnCalcExamples/ConHiddenCheckConsole/Program.cs
+++ ConnCalcExamples/ConHiddenCheckConsole/Program.cs
@@ -10,7 +10,7 @@ namespace ConnCalculatorConsole
 		{
 			Console.WriteLine("Start");
 
-			if(args.Length < 1)
+			if (args.Length < 1)
 			{
 				Console.WriteLine("The path to Idea StatiCa installation dir is required as the argument");
 				return;
@@ -24,35 +24,34 @@ namespace ConnCalculatorConsole
 
 			Console.WriteLine(string.Format("Using Idea StatiCa from '{0}'", args[0]));
 
-			using (var calcFactory = new ConnHiddenClientFactory(args[0]))
+			var calcFactory = new ConnHiddenClientFactory(args[0]);
+
+			var client = calcFactory.Create();
+
+			client.OpenProject("testProj.ideaCon");
+
+			// get detail about idea connection project
+			var projInfo = client.GetProjectInfo();
+
+			if (projInfo != null && projInfo.Connections != null)
 			{
-				var client = calcFactory.Create();
-
-				client.OpenProject("testProj.ideaCon");
-
-				// get detail about idea connection project
-				var projInfo = client.GetProjectInfo();
-
-				if (projInfo != null && projInfo.Connections != null)
+				// iterate all connections in the project
+				foreach (var con in projInfo.Connections)
 				{
-					// iterate all connections in the project
-					foreach (var con in projInfo.Connections)
-					{
-						Console.WriteLine(string.Format("Starting calculation of connection {0}", con.Identifier));
+					Console.WriteLine(string.Format("Starting calculation of connection {0}", con.Identifier));
 
-						// calculate a get results for each connection in the project
-						var conRes = client.Calculate(con.Identifier);
-						Console.WriteLine("Calculation is done");
+					// calculate a get results for each connection in the project
+					var conRes = client.Calculate(con.Identifier);
+					Console.WriteLine("Calculation is done");
 
-						// get the geometry of the connection
-						var connectionModel = client.GetConnectionModel(con.Identifier);
-					}
+					// get the geometry of the connection
+					var connectionModel = client.GetConnectionModel(con.Identifier);
 				}
-
-				client.CloseProject();
-
-				client.Close();
 			}
+
+			client.CloseProject();
+
+			client.Close();
 
 			Console.WriteLine("End");
 			var x = Console.ReadLine();
