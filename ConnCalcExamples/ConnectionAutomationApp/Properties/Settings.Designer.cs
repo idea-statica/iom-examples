b30cd0af10c85cd3ee459101e8e072c4086af7ce
mpospisil76 <m.pospisil76@gmail.com>
2020-03-04 19:17:17 +0100

    no message

--- ConnCalcExamples/ConnectionAutomationApp/Properties/Settings.Designer.cs
+++ ConnCalcExamples/ConnectionAutomationApp/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ConnectionHiddenCalculation.Properties {
+namespace ConnectionAutomationApp.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
