8bd262fad74fa61a521636dfe9e22487b981406d
Martin Pospisil <martin.pospisil@ideastatica.com>
2020-05-28 23:14:50 +0200

    write geometry of the connection to Console

--- ConnCalcExamples/ConHiddenCheckConsole/Program.cs
+++ ConnCalcExamples/ConHiddenCheckConsole/Program.cs
@@ -23,14 +23,33 @@ namespace ConnCalculatorConsole
 				return;
 			}
 
-			Console.WriteLine(string.Format("Using Idea StatiCa from '{0}'", args[0]));
+			string ideaStaticaInstallDir = args[0];
+			Console.WriteLine(string.Format("Using Idea StatiCa from '{0}'", ideaStaticaInstallDir));
 
-			var calcFactory = new ConnHiddenClientFactory(args[0]);
+			// the default idea connection project for calculation
+			string ideaConnProjectFileName = "testProj.ideaCon";
 
-			var client = calcFactory.Create();
+			if (args.Length < 2)
+			{
+				// custom idea connection project can be specified in the second argumet
+				ideaConnProjectFileName = args[1];
+
+				if(!File.Exists(ideaConnProjectFileName))
+				{
+					Console.WriteLine(string.Format("The project file '{0}' doesn't exist", ideaConnProjectFileName));
+					return;
+				}
+			}
+
+			// create the instance of factory - it looks for IDEA StatiCa in the directory 'ideaStaticaInstallDir'
+			ConnHiddenClientFactory calcFactory = new ConnHiddenClientFactory(ideaStaticaInstallDir);
+
+			//create the instance of the IDEA StatiCa Client
+			ConnectionHiddenCheckClient client = calcFactory.Create();
 			try
 			{
-				client.OpenProject("testProj.ideaCon");
+				// open project on the service side
+				client.OpenProject(ideaConnProjectFileName);
 
 				try
 				{
@@ -51,12 +70,19 @@ namespace ConnCalculatorConsole
 							var jsonSetting = new Newtonsoft.Json.JsonSerializerSettings { ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver(), Culture = CultureInfo.InvariantCulture };
 
 							var jsonFormating = Newtonsoft.Json.Formatting.Indented;
-							string jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(cbfemResults, jsonFormating, jsonSetting);
+							string resultsJsonString = Newtonsoft.Json.JsonConvert.SerializeObject(cbfemResults, jsonFormating, jsonSetting);
 
-							Console.WriteLine(jsonString);
+							Console.WriteLine(resultsJsonString);
 
 							// get the geometry of the connection
 							var connectionModel = client.GetConnectionModel(con.Identifier);
+							string geometryJsonString = Newtonsoft.Json.JsonConvert.SerializeObject(connectionModel, jsonFormating, jsonSetting);
+
+							Console.WriteLine(resultsJsonString);
+
+							Console.WriteLine("");
+							Console.WriteLine("The geometry of the calculated project");
+
 						}
 					}
 				}
